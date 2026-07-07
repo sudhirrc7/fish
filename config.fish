aec8751f593c23b9e393c68f17eedcf61431c0e3
Sudhir Chakradhar <sudhirnew37@gmail.com>
2026-07-08 00:25:02 +0530

    dont know

--- config.fish
+++ config.fish
@@ -75,7 +75,7 @@ set PATH $PATH /Users/sudhir/.local/bin
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
-alias g++ g++-15
+alias g++ g++-16
 
 # pnpm
 set -gx PNPM_HOME /Users/sudhir/Library/pnpm
